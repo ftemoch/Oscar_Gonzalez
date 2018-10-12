0b378ef5c95ffe554725dd814cb21955459e8ba1
lego8605 <lego8605@hotmail.com>
2018-10-12 10:33:47 -0500

    aqui esta el parcial 2

--- Test/Test/Controllers/HomeController.cs
+++ Test/Test/Controllers/HomeController.cs
@@ -4,27 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Test.Controllers
+namespace Parcial2.Controllers
 {
-    public class HomeController : Controller
+    public class ClienteController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Random()
+
         {
+            var Cliente = new Cliente()
+            {
+                Nombre="elvis",
+                Apellido="presley",
+                Sueldo=234,5
+            };
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
---
+++ Test/Test/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Parcial2.Models {
+    public class Cliente
+    {
+
+        public int ID { get; set; }
+        public String Nombre { get; set; }
+        public String tipo { get; set; }
+        public String Apellido { get; set; }
+        public float Sueldo { get; set; }
+    }
+}
+
+
